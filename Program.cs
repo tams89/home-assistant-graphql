a28a5bdd47609cb3c6ea143e02c1d4f452feaa49
Tamesh Sivaguru <tam.s89@gmail.com>
2025-02-25 17:37:07 +0000

    Can query entity states using GraphQL
    - installed prereq libs
    - added query to handle getting entity state from local HA instance
    - token in user secrets

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
+using home_assistant_graphql.Types;
+using HotChocolate.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddUserSecrets<Program>();
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddGraphQLServer();
+builder.Services
+    .AddOpenApi()
+    .AddHttpClient()
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,8 +20,9 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseRouting();
 app.UseHttpsRedirection();
-
 app.MapGraphQL();
+app.UsePlayground();
 
 app.Run();
---
+++ Types/Query.cs
@@ -0,0 +1,40 @@
+﻿namespace home_assistant_graphql.Types
+{
+    using HotChocolate.Types;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public class Query
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IConfiguration _config;
+
+        public Query(IHttpClientFactory clientFactory, IConfiguration config)
+        {
+            _clientFactory = clientFactory;
+            _config = config;
+        }
+
+        public async Task<string> GetEntityState(string entityId)
+        {
+
+            var client = _clientFactory.CreateClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {_config["token"]}");
+
+            var response = await client.GetAsync($"http://homeassistant.lan:8123/api/states/{entityId}");
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+
+    public class QueryType : ObjectType<Query>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Query> descriptor)
+        {
+            descriptor.Field(q => q.GetEntityState(default))
+                      .Argument("entityId", a => a.Type<NonNullType<StringType>>())
+                      .Type<StringType>();
+        }
+    }
+}
